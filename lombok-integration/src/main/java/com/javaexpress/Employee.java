cdcb26ef930ebc9d6738fbabce7ee330c95f699f
unknown <javaexpresschannel>
2021-03-07 12:27:32 +0530

    project lombok

---
+++ lombok-integration/src/main/java/com/javaexpress/Employee.java
@@ -0,0 +1,20 @@
+package com.javaexpress;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+
+
+public class Employee {
+
+	private String firstName;
+	private String lastName;
+	
+	
+}
---
+++ lombok-integration/src/main/java/com/javaexpress/Test.java
@@ -0,0 +1,11 @@
+package com.javaexpress;
+
+public class Test {
+
+	public static void main(String[] args) {
+		Employee emp = new Employee();
+		emp.setFirstName("java");
+		emp.setLastName("expresss");
+		System.out.println(emp);
+	}
+}
